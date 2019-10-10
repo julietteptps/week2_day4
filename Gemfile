4787cdd1914f336ffd764c8ec99d5c8410e4781e
Juliette Petit-Pas <petitpasjuliette@gmail.com>
2019-10-10 14:27:39 +0200

    update

---
+++ Gemfile
@@ -0,0 +1,8 @@
+ruby '2.5.1'
+gem 'rspec'
+gem 'pry'
+gem 'rubocop', '~> 0.57.2'
+gem 'dotenv'
+gem 'rubygems'
+gem 'nokogiri'
+gem 'open-uri'
---
+++ lib/cryptodark.rb
@@ -0,0 +1,27 @@
+require 'rubygems'
+require 'nokogiri'
+require 'open-uri'
+
+def crypto_scrapping(url) # Je créé une méthode "crypto_scrapping" avec comme paramètre la variable url
+
+
+	page = Nokogiri::HTML(open(url)) # Je créé la variable page qui permet à nokogiri de scrapper mon url
+	crypto_value = [] # Je créé le tableau crypto_value
+	crypto_name = [] # Je créé un deuxième tableau crypto_name
+	crypto_hash = {} # Je créé un hash crypto_hash
+
+  #Je vais chercher les noms des cryptomonnaies avec la fonction xpath puis je les ajoute à mon tableau crypto_name
+		page.xpath('//tr/td[@class = "no-wrap currency-name"]/a').each do |node|
+			crypto_name << node.text
+		end
+
+    #Je fais pareil avec les valeurs des cryptomonnaies puis je les ajoute à mon tableau crypto_value
+		page.xpath('//tr/td/a[@class = "price"]').each do |node|
+			crypto_value << node.text
+		end
+		crypto_hash = crypto_name.zip(crypto_value) # J'utilise la fonction .zip qui permet de faire suivre un nom et une valeur de cryptomonnaie à la suite
+   puts crypto_hash
+end
+
+
+crypto_scrapping('https://coinmarketcap.com/all/views/all/') # J'affiche ma méthode avec l'url donné
