2cf618b1b0a40025936d131c450fd3267cb1e00f
Juliette Petit-Pas <petitpasjuliette@gmail.com>
2019-10-10 21:52:20 +0200

    final update

--- lib/cryptodark.rb
+++ lib/cryptodark.rb
@@ -4,26 +4,38 @@ require 'open-uri'
 
 def crypto_scrapping(url) # Je créé une méthode "crypto_scrapping" avec comme paramètre la variable url
 
-
 	page = Nokogiri::HTML(open(url)) # Je créé la variable page qui permet à nokogiri de scrapper mon url
-	crypto_value = [] # Je créé un array crypto_value
-	crypto_name = [] # Je créé un deuxième array crypto_name
-	crypto_hash = {} # Je créé un hash crypto_hash
 
-  #Je vais chercher les noms des cryptomonnaies avec la fonction xpath puis je les ajoute à mon tableau crypto_name
-		page.xpath('//tr/td[@class = "no-wrap currency-name"]/a').each do |node|
-			crypto_name << node.text
-		end
+  # Je vais chercher les noms des cryptomonnaies avec la fonction xpath puis je les ajoute à mon tableau crypto_name
+  crypto_name = []
+  page.xpath('//tr/td[@class = "no-wrap currency-name"]/a').each do |node|
+		crypto_name << node.text
+	end
 
-    #Je fais pareil avec les valeurs des cryptomonnaies puis je les ajoute à mon tableau crypto_value
-		page.xpath('//tr/td/a[@class = "price"]').each do |node|
-			crypto_value << node.text
-		end
-		#crypto_hash = crypto_name.zip(crypto_value) # J'utilise la fonction .zip qui permet de faire suivre un nom et une valeur de cryptomonnaie à la suite
-   #puts crypto_hash
-   puts crypto_name + crypto_value
+  # Je fais pareil avec les valeurs des cryptomonnaies puis je les ajoute à mon tableau crypto_value
+  crypto_value = []
+  page.xpath('//tr/td/a[@class = "price"]').each do |node|
+		crypto_value << node.text
+	end
 
+  # J'associe les deux tableaux (la première valeur du tableau de nom avec la première valeur du tableau de prix, la deuxième et ainsi de suite)
+  crypto_linked = crypto_name.zip(crypto_value)
+
+  # crypto_linked = [
+  #   ["Bitcoin", "9000"],
+  #   ["Ethereum", "300"],
+  #   ...
+  # ]
+
+  return crypto_linked
 end
 
+arr_crypto = crypto_scrapping('https://coinmarketcap.com/all/views/all/')
 
-crypto_scrapping('https://coinmarketcap.com/all/views/all/') # J'affiche ma méthode avec l'url donné
+# Pour chaque crypto ...
+arr_crypto.each do |crypto|
+  # Je calcul et affiche les valeurs
+  name = crypto[0]
+  price = crypto[1]
+  puts "#{name} - #{price}"
+end
--- lib/mairiechristmas.rb
+++ lib/mairiechristmas.rb
@@ -3,9 +3,7 @@ require 'nokogiri'
 require 'open-uri'
 
 def get_townhall_email(url) # Méthode qui récupère les adresses email de chaque mairie
-
   page = Nokogiri::HTML(open(url)) # Je créé la variable page qui permet à nokogiri de scrapper mon url
-  emails_adress = ' '
   page.xpath('/html/body/div/main/section[2]/div/table/tbody/tr[4]/td[2]').each do |node| # Je récupère l'adresse mail de la mairie d'Avernes avec la fonction .xpath
     return node.text
   end
@@ -14,13 +12,21 @@ end
 def get_townhall_urls(url) # Méthode qui récupère les url de chaque mairie du Val d'Oise et qui applique la méthode get_townhall_email
 
   page = Nokogiri::HTML(open(url)) # Je créé la variable page qui permet à nokogiri de scrapper mon url
-  hash = {} # Je créé un hash
-    page.xpath('//p/a').each do |node| # Je récupère les email avec la fonction .xpath
-        html = node["href"]
-        html = "http://annuaire-des-mairies.com" + html[1..-1]
-        hash[node.text]=get_townhall_email(html)
-    end
-    puts hash
+  townUrls = {} # Objet qui contiendra mes urls
+
+  # Je récupère les balises a de la page
+  page.xpath('//p/a').each do |a|
+    href = a["href"] # Je récupère le lien dans la balise a
+    href = href[1..-1] # J'enlève le ./ au début de l'url (lien relatif)
+    link = "http://annuaire-des-mairies.com" + href # Je créé la variable link qui correspond à l'url de la ville
+    email = get_townhall_email(link) # Je créé la variable email qui correspond à l'email de la ville
+    townUrls[a.text] = email # J'ajoute une nouvelle valeur dans townUrls (clé : ville; valeur : email)
+  end
+  return townUrls
 end
 
-get_townhall_urls('http://annuaire-des-mairies.com/val-d-oise.html')
+urls = get_townhall_urls('http://annuaire-des-mairies.com/val-d-oise.html') # J'affiche ma méthode avec l'url donné
+
+urls.each_pair {|key, value|
+  puts "#{key} - #{value}"
+}
